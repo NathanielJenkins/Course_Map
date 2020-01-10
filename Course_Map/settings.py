ac67ff3bd27d8192bee0fe23dd764666e2ea75ab
NathanielJenkins <njboale@gmail.com>
2020-01-10 13:47:51 -0800

    database changes

--- Course_Map/settings.py
+++ Course_Map/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = [
 
 import django_heroku
 django_heroku.settings(locals())
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
