56da32faea46b320323cd131441e9b6297629874
NathanielJenkins <njboale@gmail.com>
2020-01-10 16:20:56 -0800

    production issues

--- Course_Map/settings.py
+++ Course_Map/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wu8vlr5d921!%7eeveq+81v1(%e%x#^6n8&%cln#6c_8pdma*w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
